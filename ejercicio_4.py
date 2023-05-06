910bcfd0cf48c69aacf365206232960bd658dbcd
abbeyblack <javypaz9510@gmail.com>
2023-05-05 22:39:03 -0400

    ejercicio 4

---
+++ ejercicio_4.py
@@ -0,0 +1,6 @@
+horas_trabajadas = input("Ingrese el numero de horas trabajadas: ")
+coste = input("Ingrese el valor de la hora trabajada: ")
+
+sueldo = int(horas_trabajadas)*int(coste)
+
+print(f"La paga que le corresponde es de {sueldo}")
