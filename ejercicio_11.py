8b66864cd13712c7a63e2d186540bc47dad4d9e3
abbeyblack <javypaz9510@gmail.com>
2023-05-07 21:33:51 -0400

    ejercicio 11

---
+++ ejercicio_11.py
@@ -0,0 +1,11 @@
+edad_minima = 16
+ingresos_tributables = 1000
+
+edad_usuario = input("¿Cuál es su edad?")
+ingreso_usuario = input("¿Cuales son sus ingresos mensuales?")
+
+if int(edad_usuario) > edad_minima:
+    int(ingreso_usuario) >= ingresos_tributables
+    print("Debe pagar impuestos")
+else:
+    print("No debe pagar impuestos")
